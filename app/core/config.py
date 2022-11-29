6174c9140ea5f664b66453735e2d786dcabbbb99
LeAries <learies@ya.ru>
2022-11-29 06:24:37 +0300

    автоматическое создание суперпользователя

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,12 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     database_url: str
     secret: str = 'Супер секретное слово – пряник'
+    first_superuser_email: None | EmailStr = None
+    first_superuser_password: None | str = None
 
     class Config:
         env_file = '.env'
