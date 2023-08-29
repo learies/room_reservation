cc4a48725f209c43f365b381b0818d84a378c576
LeAries <learies@ya.ru>
2023-08-29 05:57:36 +0300

    fix: Исправлен sessionmaker на async_sessionmaker

--- app/core/db.py
+++ app/core/db.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from typing import AsyncGenerator
 
 from sqlalchemy import Column, DateTime, Integer
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base, declared_attr
 
 from app.core.config import settings
 
@@ -35,7 +35,7 @@ Base = declarative_base(cls=PreBase)
 
 engine = create_async_engine(settings.database_url)
 
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
