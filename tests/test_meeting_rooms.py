f007a0c346ed44061bf6286e3553ba35528f01ea
LeAries <learies@ya.ru>
2023-09-03 12:22:27 +0300

    feat: Добавлен test_create_meeting

--- tests/test_meeting_rooms.py
+++ tests/test_meeting_rooms.py
@@ -14,9 +14,55 @@ def test_get_all_meeting_rooms(client: TestClient):
     При отсутствии переговорок в db, должен возвращаться пустой список.
     """
     response = client.get("/api/v1/meeting_rooms/")
-    assert (
-        response.status_code == status.HTTP_200_OK,
-    ), "При GET-запросе к эндпоинту `/api/v1/meeting_rooms/` должен возвращаться статус-код 200."
-    assert (
-        isinstance(response.json(), list),
-    ), "При GET-запросе к эндпоинту `/api/v1/meeting_rooms/` должен возвращаться объект типа `list`."
+    assert response.status_code == status.HTTP_200_OK,\
+        "При GET-запросе к эндпоинту `/api/v1/meeting_rooms/` должен возвращаться статус-код 200."
+    assert isinstance(response.json(), list),\
+        "При GET-запросе к эндпоинту `/api/v1/meeting_rooms/` должен возвращаться объект типа `list`."
+
+
+def test_create_meeting_rooms(client: TestClient):
+    """Тест POST запроса на создание переговорок.
+
+    Эндпоинт должен создать переговорку со статусом 201 и вернуть словарь.
+    """
+    response = client.post(
+        "/api/v1/meeting_rooms/",
+        json={
+            "name": "Главная переговорка",
+            "description": "Очень большая, модная и помпезная комната.",
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert isinstance(response.json(), dict)
+
+
+def test_create_meeting_rooms_which_duplicate_name(client: TestClient):
+    """Тест POST запроса на создание переговорок с существующим именем.
+
+    Эндпоинт должен вернуть ошибку 422, поле name должно быть уникальным.
+    """
+    response = client.post(
+        "/api/v1/meeting_rooms/",
+        json={
+            "name": "Главная переговорка",
+            "description": "Очень большая, модная и помпезная комната.",
+        },
+    )
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY,\
+        "При POST-запросе к эндпоинту `/api/v1/meeting_rooms/`, поле name должно быть уникальным."
+
+
+def test_create_meeting_rooms_which_max_length_name(client: TestClient):
+    """Тест POST запроса на создание переговорок с максимальной длинной имени.
+
+    Эндпоинт должен вернуть ошибку 422, если поле name > 100 символов.
+    """
+    response = client.post(
+        "/api/v1/meeting_rooms/",
+        json={
+            "name": "Г" * 101,
+            "description": "Очень большая, модная и помпезная комната.",
+        },
+    )
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY,\
+        "При POST-запросе к эндпоинту `/api/v1/meeting_rooms/`, поле name не должно быть > 100 символов."
